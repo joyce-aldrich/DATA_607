63549f6b54681de7162943dc75f76c6d1e04ab63
joyce-aldrich <112223691+joyce-aldrich@users.noreply.github.com>
2022-09-13 23:27:07 -0400

    Add files via upload

---
+++ Assignment2_Movies_Rating.sql
@@ -0,0 +1,50 @@
+
+DROP TABLE IF EXISTS Assignment;
+
+DROP TABLE Movies_Rating
+CREATE TABLE Movies_Rating
+(
+  Name varchar(100) NOT NULL,
+  Movie_Name varchar(100) NOT NULL,
+  Rating int Not NULL);
+  
+INSERT INTO Movies_Rating (Name, Movie_Name, Rating)
+Values 
+('Sharon', 'Top_Gun', 2),
+('Daniel', 'Top_Gun', 3),
+('Debra', 'Top_Gun', 5),
+('Oggi', 'Top_Gun', 4),
+('Melissa', 'Top_Gun', 2),
+('Sharon', 'Thor', 4),
+('Daniel', 'Thor', 4),
+('Debra', 'Thor', 2),
+('Oggi', 'Thor', 3),
+('Melissa', 'Thor', 3),
+('Sharon', 'Lightyear', 3),
+('Daniel', 'Lightyear', 3),
+('Oggi', 'Lightyear', 2),
+('Melissa', 'Lightyear', 5),
+('Sharon', 'Minions', 4),
+('Daniel', 'Minions', 4),
+('Debra', 'Minions', 1),
+('Oggi', 'Minions', 2),
+('Melissa', 'Minions', 5),
+('Sharon', 'Doctor_Strange', 3),
+('Daniel', 'Doctor_Strange', 4),
+('Debra', 'Doctor_Strange', 2),
+('Oggi', 'Doctor_Strange', 3),
+('Sharon', 'Spider_Man', 3),
+('Daniel', 'Spider_Man', 3),
+('Oggi', 'Spider_Man', 2),
+('Melissa', 'Spider_Man', 4);
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
